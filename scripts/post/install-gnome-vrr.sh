904c315cbba24f88585df2206674038abefb00d3
Avi <nektworks@outlook.com>
2023-07-27 15:12:22 +0200

    Get rid of &&\ in sh file

--- scripts/post/install-gnome-vrr.sh
+++ scripts/post/install-gnome-vrr.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:kylegospo:gnome-vrr mutter gnome-control-center gnome-control-center-filesystem xorg-x11-server-Xwayland && \
+rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:kylegospo:gnome-vrr mutter gnome-control-center gnome-control-center-filesystem xorg-x11-server-Xwayland
