8300f165109b88962e62ab8b1d18821c7ad7c78c
Avi <nektworks@outlook.com>
2023-08-24 16:39:18 +0200

    Add special laptop image.
    This image includes NVIDIA drivers and does not include GNOME VRR.

--- scripts/install-gnome-vrr.sh
+++ scripts/install-gnome-vrr.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
 rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:kylegospo:gnome-vrr mutter mutter-common gnome-control-center gnome-control-center-filesystem xorg-x11-server-Xwayland
